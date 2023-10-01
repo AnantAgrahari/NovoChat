488c2ac5159aa1492ec7456127cd70064ba2aa21
Anant <aradhyaanant1@gmail.com>
2023-10-02 02:50:48 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,14 @@ const express =require("express");
 const dotenv=require("dotenv");
 const { chats }= require("./data/data");
 const app=express();
+const userRoutes=require('./routes/userRoutes')
 
 dotenv.config();
 app.get("/",(req,res)=>{
  res.send("api is running");
 });
-app.get("/api/chat",(req,res)=>{
-    res.send(chats);
-});
 
-app.get("/api/chat/:id",(req,res)=>{
-    const singleChat=chats.find((c)=>c._id===req.params.id);
-    res.send(singleChat);
-});
+app.use('/api/user',userRoutes)
+
 const PORT=process.env.PORT || 5000;
 app.listen(5000,console.log(`server started on port ${PORT}`));
