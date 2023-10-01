6666c1c9f1f937f1482f68a0aa39d7d99dd174d7
Anant <aradhyaanant1@gmail.com>
2023-10-01 15:25:42 +0530

    updated models

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,2 +1,29 @@
 //chat name
 //isgroupchat//users//latestmessage//groupadmin(if group chat then only)
+const mongoose=require("mongoose");
+const chatModel=mongoose.Schema(
+    {
+        chatName:{type:String,trim:true},   //1 to 1 //
+        isGroupChat:{type:Boolean,default:false},
+        users:[                                                  //1 to 1//
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User",
+            },
+        ],
+        latestMessage:{                                 //1 to 1//
+            type:mongoose.Schema.Types.ObjectId,             //latest message which needs to be displayed on the screen//
+            ref:"Message",
+        },                 //chatModel means for one single chat,be it 1 to 1 or group//
+        groupAdmin:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        },
+    },
+    {
+        timestamps:true,                   //every time a new chat is added//     
+    }
+);
+
+const Chat=mongoose.model("Chat",chatModel);
+module.exports=Chat;
---
+++ backend/models/messageModel.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose");
+const messageModel=mongoose.Schema(
+    {
+      sender:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
+      content:{type:String,trim:true},      //here the content is the message that the sender sends//
+      chat:{type:mongoose.Schema.Types.ObjectId,ref:"Chat"},
+    },
+    {
+        timestamps:true,
+    }
+);
+const Message=mongoose.model("Message",messageModel);
+module.exports=Message;
