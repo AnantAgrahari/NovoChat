1a44ddedb1a86bf4a5aafd2a43a185d325cc4574
Anant <aradhyaanant1@gmail.com>
2023-10-02 15:39:55 +0530

    commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const {registerUser}=require("../controllers/userControllers");
 
 router.route("/").post(registerUser);
+router.post('/login',authUser);
 
 
 module.exports=router;
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,18 @@ const dotenv=require("dotenv");
 const { chats }= require("./data/data");
 const app=express();
 const userRoutes=require('./routes/userRoutes')
-
+const {notFound,errorHandler}=require("./middleware/errorMiddleware");
 dotenv.config();
+
+app.use(express.json());          //to accept json data//
 app.get("/",(req,res)=>{
  res.send("api is running");
 });
 
 app.use('/api/user',userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT=process.env.PORT || 5000;
 app.listen(5000,console.log(`server started on port ${PORT}`));
