8fc9f19458b3b3bb18b2fb82ed36b6952490176b
Anant <aradhyaanant1@gmail.com>
2023-10-03 23:57:36 +0530

    final chat api

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -158,4 +158,30 @@ const renameGroup = asyncHandler(async (req, res) => {
   });
 
 
-module.exports={accessChat,fetchChats,createGroupChat,renameGroup};
+  const removeFromGroup = asyncHandler(async (req, res) => {
+    const { chatId, userId } = req.body;
+  
+    // check if the requester is admin
+  
+    const removed = await Chat.findByIdAndUpdate(
+      chatId,
+      {
+        $pull: { users: userId },
+      },
+      {
+        new: true,
+      }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+  
+    if (!removed) {
+      res.status(404);
+      throw new Error("Chat Not Found");
+    } else {
+      res.json(removed);
+    }
+  });
+
+
+module.exports={accessChat,fetchChats,createGroupChat,renameGroup,addToGroup,removeFromGroup};
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {accessChat,fetchChats,createGroupChat,renameGroup} =require('../controllers/chatControllers')
+const {accessChat,fetchChats,createGroupChat,renameGroup,addToGroup,removeFromGroup} =require('../controllers/chatControllers')
 const {
   accessChat,
   fetchChats,
