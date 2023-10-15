9024d60482b431e12750bef369f0c6695d9d0aca
Anant <aradhyaanant1@gmail.com>
2023-10-15 23:38:06 +0530

    commit

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -1,5 +1,7 @@
 const asyncHandler=require("express-async-handler");
-const Message=require('../models/messageModel')
+const Message=require('../models/messageModel');
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
 const sendMessage=asyncHandler(async(req,res)=>{
 
   const {content,chatId}=req.body;
@@ -18,14 +20,28 @@ const sendMessage=asyncHandler(async(req,res)=>{
 
    try {
     var message=await Message.create(newMessage);       
-
+   
+    message=await message.populate("sender","name pic");
+    message=await message.populate("chat");
+    message=await User.populate(message,{
+        path:"chat.users",
+        select:"name pic email",
+    });
+    await Chat.findByIdAndUpdate(req.body.chatId,{
+        latestMessage:message,
+    });
+    res.json(message);
 
 
 
    } catch (error) {
     
+    res.status(400);
+    throw new Error(error.message);
    }
 
 
 });
+
+
 module.exports={sendMessage};
